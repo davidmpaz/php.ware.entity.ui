b73cb8b79a310c46f4d7c82fdeaa6bf6acd725e0
David Moises Paz Reyes <davidmpaz@gmail.com>
2011-04-04 14:32:13 -0400

    Implements outline labeling feature
    Add respectives icons and code to get a customized ouline in dsl editor

--- src/php/ware/ui/EntityUiModule.java
+++ src/php/ware/ui/EntityUiModule.java
@@ -9,7 +9,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  * Use this class to register components to be used within the IDE.
  */
 public class EntityUiModule extends php.ware.ui.AbstractEntityUiModule {
-	public EntityUiModule(AbstractUIPlugin plugin) {
-		super(plugin);
-	}
+    public EntityUiModule(AbstractUIPlugin plugin) {
+        super(plugin);
+    }
 }
